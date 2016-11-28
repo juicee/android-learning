2f15c4148733ed1dceb95cfb4bfcb6a7ffeb2276
parkerzhu <zhuxinbin2000@gmail.com>
2016-11-28 13:46:43 +0800

    更新

--- RemoteControl/app/src/main/java/com/tencent/remotecontrol/RemoteControlActivity.java
+++ RemoteControl/app/src/main/java/com/tencent/remotecontrol/RemoteControlActivity.java
@@ -1,12 +1,20 @@
 package com.tencent.remotecontrol;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class RemoteControlActivity extends SingleFragmentActivity {
     @Override
     protected Fragment CreateFragment() {
         return new RemoteControlFragment();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
     }
 }
